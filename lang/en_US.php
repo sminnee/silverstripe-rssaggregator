2ead0230c9139e186289f17b3189d99cd0beae9b
Ingo Schommer <ingo@silverstripe.com>
2008-11-01 15:30:17 +0000

    MINOR Added language master table

---
+++ lang/en_US.php
@@ -0,0 +1,96 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['RSSAggEntry']['PLURALNAME'] = array(
+	'R S S Agg Entries',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['RSSAggEntry']['SINGULARNAME'] = array(
+	'R S S Agg Entry',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['RSSAggEntry']['db_Content'] = array(
+	'Content',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggEntry']['db_Date'] = array(
+	'Date',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggEntry']['db_Displayed'] = array(
+	'Displayed',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggEntry']['db_EnclosureURL'] = array(
+	'EnclosureURL',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggEntry']['db_Permalink'] = array(
+	'Permalink',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggEntry']['db_Title'] = array(
+	'Title',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggSource']['PLURALNAME'] = array(
+	'RSS Sources',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['RSSAggSource']['SINGULARNAME'] = array(
+	'RSS Source',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['RSSAggSource']['db_LastChecked'] = array(
+	'LastChecked',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggSource']['db_RSSFeed'] = array(
+	'RSSFeed',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggSource']['db_Title'] = array(
+	'Title',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggregatingPage']['PLURALNAME'] = array(
+	'R S S Aggregating Pags',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['RSSAggregatingPage']['SINGULARNAME'] = array(
+	'R S S Aggregating Page',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['RSSAggregatingPage']['db_NumberOfItems'] = array(
+	'NumberOfItems',
+	50,
+	'Name of the object property, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggregatingPage']['has_many_Entries'] = array(
+	'Entries',
+	50,
+	'Name of an object relation, mainly used for automatically generating forms'
+);
+$lang['en_US']['RSSAggregatingPage']['has_many_SourceFeeds'] = array(
+	'SourceFeeds',
+	50,
+	'Name of an object relation, mainly used for automatically generating forms'
+);
+
+?>
