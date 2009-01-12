13cd63209ddd9627b1684047596edb9e1f60e6aa
Ingo Schommer <ingo@silverstripe.com>
2009-01-12 05:07:50 +0000

    MINOR Updated language master tables

--- lang/en_US.php
+++ lang/en_US.php
@@ -12,36 +12,6 @@ $lang['en_US']['RSSAggEntry']['SINGULARNAME'] = array(
 	50,
 	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
 );
-$lang['en_US']['RSSAggEntry']['db_Content'] = array(
-	'Content',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggEntry']['db_Date'] = array(
-	'Date',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggEntry']['db_Displayed'] = array(
-	'Displayed',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggEntry']['db_EnclosureURL'] = array(
-	'EnclosureURL',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggEntry']['db_Permalink'] = array(
-	'Permalink',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggEntry']['db_Title'] = array(
-	'Title',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
 $lang['en_US']['RSSAggSource']['PLURALNAME'] = array(
 	'RSS Sources',
 	50,
@@ -52,21 +22,6 @@ $lang['en_US']['RSSAggSource']['SINGULARNAME'] = array(
 	50,
 	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
 );
-$lang['en_US']['RSSAggSource']['db_LastChecked'] = array(
-	'LastChecked',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggSource']['db_RSSFeed'] = array(
-	'RSSFeed',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggSource']['db_Title'] = array(
-	'Title',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
 $lang['en_US']['RSSAggregatingPage']['PLURALNAME'] = array(
 	'R S S Aggregating Pags',
 	50,
@@ -76,21 +31,6 @@ $lang['en_US']['RSSAggregatingPage']['SINGULARNAME'] = array(
 	'R S S Aggregating Page',
 	50,
 	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
-);
-$lang['en_US']['RSSAggregatingPage']['db_NumberOfItems'] = array(
-	'NumberOfItems',
-	50,
-	'Name of the object property, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggregatingPage']['has_many_Entries'] = array(
-	'Entries',
-	50,
-	'Name of an object relation, mainly used for automatically generating forms'
-);
-$lang['en_US']['RSSAggregatingPage']['has_many_SourceFeeds'] = array(
-	'SourceFeeds',
-	50,
-	'Name of an object relation, mainly used for automatically generating forms'
 );
 
 ?>
